8b2dacb00e1af4f47fe4d48adfa1d74d14f1bab9
Aidan Glickman <aidan@aidanglickman.com>
2021-10-03 14:00:55 -0500

    finish linear image

--- pixel.py
+++ pixel.py
@@ -1,46 +1,66 @@
+#!/usr/bin/env python3
 import requests
 from PIL import Image
-import time
-import random
-import sys
+import abc
 import argparse
 import numpy as np
 
+BASE_URL = "http://pixel.acm.illinois.edu/"
 class ImageClass:
+    __metaclass__ = abc.ABCMeta
     IMG_HEIGHT = 128
     IMG_WIDTH = 128
 
     @staticmethod
     def getPixels(imgPath):
+        """
+        Returns a numpy array representing the pixels of the image, reshaped to the size of pixel.
+        @TODO implement alternative resize methods
+        """
         img = Image.open(imgPath).resize((ImageClass.IMG_HEIGHT, ImageClass.IMG_WIDTH))
         return np.array(img)
+    
+    @staticmethod
+    def toHex(pixel):
+        return ('#{:02X}{:02X}{:02X}').format(pixel[0], pixel[1], pixel[2])
 
-    def __init__(self, imgPath, transThresh):
+    def __init__(self, imgPath, transThresh, dry):
         self.imgPath = imgPath
         self.pixels = ImageClass.getPixels(imgPath)
         self.transThresh = transThresh
+        self.dry = dry
+
+    @abc.abstractmethod
+    def draw(self):
+        """
+        Send POST requests to actually draw the image to pixel.
+        """
+        return
+
+class LinearImage(ImageClass):
+    def __init__(self, imgPath, transThresh=100, dry=False):
+        super().__init__(imgPath, transThresh, dry)
+    
+    def draw(self):
+        for y, row in enumerate(self.pixels):
+            for x, pixel in enumerate(row):
+                if(pixel[3] > self.transThresh):
+                    pixelStr = ImageClass.toHex(pixel)
+                    requests.post(BASE_URL,  data=dict(x=x, y=y, color=pixelStr))
 
 
-def rgb_to_hex(r, g, b):
-    return ('#{:02X}{:02X}{:02X}').format(r, g, b)
+TYPE_NAMES = {"image_linear": LinearImage}
 
-def postImg():
-    img = Image.open("cutflag.png")
-    for x in range(img.width):
-        for y in range(img.height):
-            pixel = img.getpixel((x,y))
-            if pixel[3] > 100:
-                hexed = rgb_to_hex(pixel[0], pixel[1], pixel[2])
-                requests.post("http://pixel.acm.illinois.edu/", data=dict(x=x, y=y, color=hexed))
-
-def colorScale():
-    for x in range(128):
-        for y in range(128):
-            hexed = rgb_to_hex(random.randint(0, x+y), random.randint(0, x+y), random.randint(0, x+y))
-            print(f"({x}, {y}): {hexed}")
-            requests.post("http://pixel.acm.illinois.edu/", data=dict(x=x, y=y, color=hexed))
-
+def drawDriver(args):
+    imageDriver = TYPE_NAMES[args.method[0]](args.file[0])
+    imageDriver.draw()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Interact with ACM Pixel.")
-    parser.add_argument("method", metavar="M", action="store", nargs=1, default="image_linear")
+    
+    parser.add_argument("--file", "-F", dest="file", metavar="FileName", action="store", nargs=1)
+    parser.add_argument("--method", "-M", dest="method", metavar="Method", action="store", nargs=1, default="image_linear")
+    
+    args = parser.parse_args()
+    drawDriver(args)
+
